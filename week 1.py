e8c29e912ece0c0533642f39936c08bb813cf8ce
surya <suryaa.net.in@gmail.com>
2025-07-04 15:30:50 +0100

    week1-2

---
+++ week 1.py
@@ -0,0 +1,21 @@
+# why vectors and matrices 
+"""
+Artificial intelligence
+computer graphics
+computer games
+data science 
+robotics 
+machine learning
+optimisation
+
+"""
+"""
+A vector has both magnitude and direction ex- force momentum velocity acceleration
+A scalar has only magnitude(its just a single number) ex- mass, time, temp, distance, speed
+1. Cartisian cordinate system 
+2.distance between points
+3. Linear algeabra
+4.square matrix 
+5.scalar multiplication
+"""
+# numpy arrays 1d vs 2d
