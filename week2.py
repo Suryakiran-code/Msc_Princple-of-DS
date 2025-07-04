39844766ffdbea6e492b6fdb1327e78387146bdd
surya <suryaa.net.in@gmail.com>
2025-07-04 21:55:54 +0100

    added

--- week2.py
+++ week2.py
@@ -1,3 +1,12 @@
+"""
+Applications of Vectors and Matrices
+Useful applications of vectors (lines, dot product)
+Matrix transformations, rotations, reflections, determinant
+Eigenvalues and eigenvectors
+Visualising matrix transformations using matplotlib
+Matrix decompositions
+"""
+
 # Eigenvectors and Probability
 
 # 1. Introduction to probability
